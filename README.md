ddb6ac67830a90db17746c6fb12f34b0fa96d40c
Awilum <awilum@msn.com>
2020-03-22 15:40:08 +0300

    feat(core): initial code commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# form-admin-noir
-Forms manager for Flextype Admin Panel Noir.
+# Form Admin Plugin for [Flextype](http://flextype.org/)
+![MIT License](https://img.shields.io/badge/license-MIT-blue.svg?style=flat-square)
+
+Form Admin Plugin to manage user forms in Flextype Admin Panel.
+
+## LICENSE
+[The MIT License (MIT)](https://github.com/flextype-plugins/form-admin/blob/master/LICENSE.txt)
+Copyright (c) 2018-2020 [Sergey Romanenko](https://github.com/Awilum)
---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Flextype;
+
+$app->group('/' . $admin_route, function () use ($app) : void {
+
+    // FieldsetsController
+    $app->get('/fieldsets', 'FieldsetsController:index')->setName('admin.fieldsets.index');
+    $app->get('/fieldsets/add', 'FieldsetsController:add')->setName('admin.fieldsets.add');
+    $app->post('/fieldsets/add', 'FieldsetsController:addProcess')->setName('admin.fieldsets.addProcess');
+    $app->get('/fieldsets/edit', 'FieldsetsController:edit')->setName('admin.fieldsets.edit');
+    $app->post('/fieldsets/edit', 'FieldsetsController:editProcess')->setName('admin.fieldsets.editProcess');
+    $app->get('/fieldsets/rename', 'FieldsetsController:rename')->setName('admin.fieldsets.rename');
+    $app->post('/fieldsets/rename', 'FieldsetsController:renameProcess')->setName('admin.fieldsets.renameProcess');
+    $app->post('/fieldsets/duplicate', 'FieldsetsController:duplicateProcess')->setName('admin.fieldsets.duplicateProcess');
+    $app->post('/fieldsets/delete', 'FieldsetsController:deleteProcess')->setName('admin.fieldsets.deleteProcess');
+
+})->add(new AdminPanelAuthMiddleware($flextype));
