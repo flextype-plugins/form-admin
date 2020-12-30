ddddada68c17f026e6b65749680249a214e54bcd
Awilum <awilum@msn.com>
2020-12-30 12:03:04 +0300

    Form Admin 1.6.0

--- routes/web.php
+++ routes/web.php
@@ -6,15 +6,15 @@ use Flextype\Plugin\Acl\Middlewares\AclIsUserLoggedInMiddleware;
 use Flextype\Plugin\Acl\Middlewares\AclIsUserLoggedInRolesInMiddleware;
 
 flextype()->group('/' . $admin_route, function () {
-    flextype()->get('/fieldsets', 'FieldsetsController:index')->setName('admin.fieldsets.index');
-    flextype()->get('/fieldsets/add', 'FieldsetsController:add')->setName('admin.fieldsets.add');
-    flextype()->post('/fieldsets/add', 'FieldsetsController:addProcess')->setName('admin.fieldsets.addProcess');
-    flextype()->get('/fieldsets/edit', 'FieldsetsController:edit')->setName('admin.fieldsets.edit');
-    flextype()->post('/fieldsets/edit', 'FieldsetsController:editProcess')->setName('admin.fieldsets.editProcess');
-    flextype()->get('/fieldsets/rename', 'FieldsetsController:rename')->setName('admin.fieldsets.rename');
-    flextype()->post('/fieldsets/rename', 'FieldsetsController:renameProcess')->setName('admin.fieldsets.renameProcess');
-    flextype()->post('/fieldsets/duplicate', 'FieldsetsController:duplicateProcess')->setName('admin.fieldsets.duplicateProcess');
-    flextype()->post('/fieldsets/delete', 'FieldsetsController:deleteProcess')->setName('admin.fieldsets.deleteProcess');
+    flextype()->get('/fieldsets', 'FormAdminFieldsetsController:index')->setName('admin.fieldsets.index');
+    flextype()->get('/fieldsets/add', 'FormAdminFieldsetsController:add')->setName('admin.fieldsets.add');
+    flextype()->post('/fieldsets/add', 'FormAdminFieldsetsController:addProcess')->setName('admin.fieldsets.addProcess');
+    flextype()->get('/fieldsets/edit', 'FormAdminFieldsetsController:edit')->setName('admin.fieldsets.edit');
+    flextype()->post('/fieldsets/edit', 'FormAdminFieldsetsController:editProcess')->setName('admin.fieldsets.editProcess');
+    flextype()->get('/fieldsets/rename', 'FormAdminFieldsetsController:rename')->setName('admin.fieldsets.rename');
+    flextype()->post('/fieldsets/rename', 'FormAdminFieldsetsController:renameProcess')->setName('admin.fieldsets.renameProcess');
+    flextype()->post('/fieldsets/duplicate', 'FormAdminFieldsetsController:duplicateProcess')->setName('admin.fieldsets.duplicateProcess');
+    flextype()->post('/fieldsets/delete', 'FormAdminFieldsetsController:deleteProcess')->setName('admin.fieldsets.deleteProcess');
 
 })->add(new AclIsUserLoggedInMiddleware(['redirect' => 'admin.accounts.login']))
   ->add(new AclIsUserLoggedInRolesInMiddleware(['redirect' => (flextype()->getContainer()->acl->isUserLoggedIn() ? 'admin.accounts.no-access' : 'admin.accounts.login'),
